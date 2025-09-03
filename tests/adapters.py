20427677f311a54cd1b6abaf35a97c543b8ae88d
Yihan Chen <yihanc@YihandeMacBook-Air.local>
2025-09-03 19:24:42 +0800

    [Feat] Add silu in adaptor.

--- tests/adapters.py
+++ tests/adapters.py
@@ -404,7 +404,8 @@ def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
         Float[Tensor,"..."]: of with the same shape as `in_features` with the output of applying
         SiLU to each element.
     """
-    raise NotImplementedError
+    silu = cs336_basics.nn_basic.SiLu()
+    return silu(in_features)
 
 
 def run_get_batch(
